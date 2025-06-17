013985aa0985e4294a05b53ac5a30f1fe5988551
John Lloyd Quizeo <johnkiezquizeo@gmail.com>
2025-06-18 02:38:51 +0800

    trial

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -32,7 +32,7 @@ export const handleConversation = async (req, res) => {
     const aiResponse = openRouterResponse.data.choices[0].message.content;
     console.log("Using API Key:", process.env.ELEVENLABS_API_KEY);
     const elevenlabs = new ElevenLabsClient({
-      apiKey: process.env.ELEVENLABS_API_KEY, // Make sure this is set in your .env file
+      apiKey: "sk_ec66089354d83a7d3ea83e194aa55ed15b7591f3beb87d7e", // Make sure this is set in your .env file
     });
 
     // Convert text to speech
